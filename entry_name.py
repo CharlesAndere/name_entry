ee566ea6cfdf5e43d98b8c92e58466d8dfc5def6
CharlesAndere <charlesandere41@gmail.com>
2024-03-15 18:16:26 +0300

    simple name entry limiting to three person

---
+++ entry_name.py
@@ -0,0 +1,31 @@
+# first person name
+print("first person")
+first_name = input("Enter first name: ")
+middle_name = input("Enter middle name: ")
+last_name = input("Enter last name: ")
+
+name = [first_name, middle_name, last_name]
+
+for i in range(1):
+    print(name)
+# second person
+print("second person")
+first_name = input("Enter first name: ")
+middle_name = input("Enter middle name: ")
+last_name = input("Enter last name: ")
+
+name = [first_name, middle_name, last_name]
+for i in range(1):
+    print(name)
+# third and lasst person
+print("third and last person")
+first_name = input("Enter first name: ")
+middle_name = input("Enter middle name: ")
+last_name = input("Enter last name: ")
+
+name = [first_name, middle_name, last_name]
+for i in range(1):
+    print(name)
+else:
+    print("already entered three names, its finished")
+
